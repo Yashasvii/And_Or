2ff718d1f17cc3cf5e3993fa2a895cec63b40967
Yashasvii <yashpant20@gmail.com>
2015-02-24 08:25:02 +0545

    First

---
+++ src/main/java/And.java
@@ -0,0 +1,25 @@
+/**
+ * Created by yashasvi on 2/23/15.
+ */
+import java.io.FileReader;
+import java.util.Arrays;
+public class And {
+    private String file1;
+    private String file2;
+
+    public String getFile1() {
+        return file1;
+    }
+
+    public void setFile1(String file1) {
+        this.file1 = file1;
+    }
+
+    public String getFile2() {
+        return file2;
+    }
+
+    public void setFile2(String file2) {
+        this.file2 = file2;
+    }
+}
---
+++ src/main/java/Or.java
@@ -0,0 +1,43 @@
+/**
+ * Created by yashasvi on 2/23/15.
+ */
+public class Or {
+
+    private String file1;
+    private   String complementFile1;
+    private   String file2;
+    private String complementFile2;
+
+    public String getFile1() {
+        return file1;
+    }
+
+    public void setFile1(String file1) {
+        this.file1 = file1;
+    }
+
+    public String getComplementFile1() {
+        return complementFile1;
+    }
+
+    public void setComplementFile1(String complementFile1) {
+        this.complementFile1 = complementFile1;
+    }
+
+    public String getFile2() {
+        return file2;
+    }
+
+    public void setFile2(String file2) {
+        this.file2 = file2;
+    }
+
+    public String getComplementFile2() {
+        return complementFile2;
+    }
+
+    public void setComplementFile2(String complementFile2) {
+        this.complementFile2 = complementFile2;
+    }
+}
+
